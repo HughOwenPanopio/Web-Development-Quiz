b7f95433e4e4e511f7481f1929804368c2d42826
HughOwenPanopio <panopiohughowen@gmail.com>
2024-05-29 18:52:22 +0800

    data fetching od fake api in port 9000 using useEffect, include loading animation on LoadingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import WelcomePage from './components/WelcomePage'
+import LoadingPage from './components/LoadingPage'
 
 function App() {
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log('Error fetching data', error))
+  }, [])
+
   return (
     <>
       <Header />
 
       <Main>
+        <LoadingPage />
         <WelcomePage />
       </Main>
     </>
---
+++ src/components/LoadingPage/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function LoadingPage() {
+  return (
+    <div className="loader-container">
+      <h1>Loading Questions</h1>
+      <div className="spinner-3"></div>
+    </div>
+  )
+}
+
+export default LoadingPage
