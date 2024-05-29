d0989ea5805414c8c8e06f1f7860bc287fec90f2
HughOwenPanopio <panopiohughowen@gmail.com>
2024-05-29 17:22:36 +0800

    adjusting the responsiveness of header, webpage and button

--- src/components/WelcomePage/index.jsx
+++ src/components/WelcomePage/index.jsx
@@ -5,6 +5,7 @@ function WelcomePage() {
     <div className="welcome-page">
       <h2>Welcome to the Web Development Quiz!</h2>
       <h3>x questions to test your web development mastery</h3>
+      <button className="btn-welcome">Start Quiz!</button>
     </div>
   )
 }
